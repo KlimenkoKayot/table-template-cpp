60b52547c14a70da68fd075c6f9d6af8a94c4d7b
Klimenko George <george_klimenko6@mail.ru>
2024-10-25 21:41:58 +0300

    Initial commit

---
+++ table-template-cpp.cpp
@@ -0,0 +1,32 @@
+﻿#include "table-template-cpp.h"
+
+template<typename T>
+Table<T>::Table(size_t n, size_t m) {
+	data.resize(n, std::vector<T>(m));
+}
+
+template <typename T>
+const std::vector<T>& Table<T>::operator[](size_t i) const {
+	return data[i];
+}
+
+template <typename T>
+std::vector<T>& Table<T>::operator[](size_t i) {
+	return data[i];
+}
+
+template <typename T>
+void Table<T>::resize(size_t n, size_t m) {
+	data.resize(n);
+	for (size_t i = 0; i < n; ++i) {
+		data[i].resize(m);
+	}
+}
+
+template <typename T>
+std::pair<size_t, size_t> Table<T>::size() const {
+	if (data.empty()) {
+		return std::make_pair(0, 0);
+	}
+	return std::make_pair(data.size(), data[0].size());
+}
---
+++ table-template-cpp.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class Table {
+private:
+	std::vector<std::vector<T>> data;
+
+public:
+	Table(size_t n, size_t m);
+	void resize(size_t n, size_t m);
+	std::pair<size_t, size_t> size() const;
+
+	const std::vector<T>& operator[](size_t i) const;
+	std::vector<T>& operator[](size_t i);
+};
